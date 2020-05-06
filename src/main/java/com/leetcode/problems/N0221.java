92b31c25ecd0dc2289a5448fdb163a291e0a6eb1
chenguichao <1983210400@qq.com>
2020-05-06 23:48:31 +0800

    feat:N0221

--- src/main/java/com/leetcode/problems/N0221.java
+++ src/main/java/com/leetcode/problems/N0221.java
@@ -1,5 +1,11 @@
 package com.leetcode.problems;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @Description
  *      在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
@@ -24,12 +30,29 @@ package com.leetcode.problems;
 public class N0221 {
 
 
+
     public int maximalSquare(char[][] matrix) {
+        return matrix_x_y(matrix,0,0);
+    }
+
+    private int matrix_x_y(char[][] matrix, int i, int i1) {
 
         return 0;
     }
 
     public static void main(String[] args) {
+        N0221 n0221 = new N0221();
+        Scanner sc = new Scanner(System.in);
+        int MAX = sc.nextInt();
+        String input = null;
+        char[][] matrix = new char[MAX][MAX];
+        for (int i = 0; i < MAX; i++) {
+            for (int j = 0; j < MAX; j++) {
+                matrix[j][i]  = (char) (sc.nextInt()+'0');
+            }
+        }
+        ArrayList<char[]> chars = Lists.newArrayList(matrix);
+//        n0221.maximalSquare(matrix);
 
     }
 
